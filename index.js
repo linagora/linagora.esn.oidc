1fcbe2f2a4bf6a7eaeba57eca0d1bb73d728b15e
Dang Dien PHAN <ddphan@linagora.com>
2017-09-25 08:32:53 +0700

    #2: add grunt i18n and server i18n    Add grunt i18n.    Change i18n translate to server.

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const myAwesomeModule = new AwesomeModule(MODULE_NAME, {
     new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.logger', 'logger'),
     new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.webserver.wrapper', 'webserver-wrapper'),
     new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.db', 'db'),
-    new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.webserver.middleware.authorization', 'authorizationMW')
+    new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.webserver.middleware.authorization', 'authorizationMW'),
+    new Dependency(Dependency.TYPE_NAME, 'linagora.esn.core.i18n', 'i18n')
   ],
 
   states: {
