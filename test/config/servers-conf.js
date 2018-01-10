208f3854af4ed597dee7662f467bcdbcd4bc09df
Dang Dien PHAN <ddphan@linagora.com>
2018-01-10 16:48:27 +0700

    #7 - Use Docker pipeline

--- test/config/servers-conf.js
+++ test/config/servers-conf.js
@@ -1,43 +1,31 @@
 'use strict';
 
 const DEFAULT_PORTS = {
-  express: 23455,
-  mongo: 23456,
-  redis: 23457
+  express: 23455
 };
-const tmp = 'tmp';
-const host = process.env.HOSTNAME || process.env.DOCKER_HOST || 'localhost';
-const dbName = 'tests';
-const mongoPort = process.env.PORT_MONGODB || DEFAULT_PORTS.mongo;
 
 module.exports = {
-  tmp,
-
-  default_ports: DEFAULT_PORTS,
-
-  host,
-
+  host: process.env.HOSTNAME || process.env.DOCKER_HOST || 'localhost',
   express: {
     port: process.env.PORT_EXPRESS || DEFAULT_PORTS.express
   },
-
   redis: {
-    cmd: process.env.CMD_REDIS || 'redis-server',
-    port: process.env.PORT_REDIS || DEFAULT_PORTS.redis,
-    conf_file: '',
-    log_path: '',
-    pwd: ''
+    host: 'redis',
+    port: 6379,
+    url: 'redis://redis:6379'
   },
-
   mongodb: {
-    cmd: process.env.CMD_MONGODB || 'mongod',
-    port: mongoPort,
-    interval_replica_set: process.env.MONGODB_INTERVAL_REPLICA_SET || 1000,
-    tries_replica_set: process.env.MONGODB_TRIES_REPLICA_SET || 20,
-    connectionString: 'mongodb://' + host + ':' + mongoPort + '/' + dbName,
-    replicat_set_name: 'rs',
-    dbname: dbName,
-    dbpath: tmp + '/mongo/',
-    logpath: ''
+    host: 'mongo',
+    port: 27017,
+    connectionString: 'mongodb://mongo/tests'
+  },
+  elasticsearch: {
+    host: 'elasticsearch',
+    port: 9200
+  },
+  rabbitmq: {
+    host: 'rabbitmq',
+    port: 5672,
+    url: 'amqp://rabbitmq:5672'
   }
 };
--- test/midway-backend/webserver/api/example.js
+++ test/midway-backend/webserver/api/example.js
@@ -35,7 +35,10 @@ describe('The example API', function() {
   });
 
   afterEach(function(done) {
-    this.helpers.mongo.dropDatabase(done);
+    this.helpers.mongo.dropDatabase(err => {
+      if (err) return done(err);
+      this.testEnv.core.db.mongo.mongoose.connection.close(done);
+    });
   });
 
   describe('GET /example', function() {
