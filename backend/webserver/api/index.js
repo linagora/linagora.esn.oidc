c4472d1ea84df0747d1df2a941d7fa55c481e8c0
Fabien MOYON <fmoyon@linagora.com>
2019-04-03 15:13:37 +0200

    #2707 Disabling seed module from configuration

--- backend/webserver/api/index.js
+++ backend/webserver/api/index.js
@@ -5,8 +5,9 @@ const express = require('express');
 module.exports = function(dependencies, lib) {
 
   const router = express.Router();
+  const moduleName = 'linagora.esn.seed';
 
-  require('./example')(dependencies, lib, router);
+  require('./example')(dependencies, lib, router, moduleName);
 
   return router;
 };
